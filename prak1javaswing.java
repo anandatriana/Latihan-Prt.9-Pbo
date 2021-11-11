20d72ac3c088ab2fff3b5bd825822f3563c45a37
anandatriana <90240805+anandatriana@users.noreply.github.com>
2021-11-11 17:21:26 +0800

    Add files via upload

---
+++ prak1javaswing.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class prak1javaswing {
+	
+	public static void main(String[] args) {
+		
+		JFrame f = new JFrame("title");
+		JPanel p = new JPanel();
+		JButton b = new JButton("press me");
+		
+		p.add(b);
+		f.setContentPane(p);
+		
+		f.show();
+	}
+	
+	}
